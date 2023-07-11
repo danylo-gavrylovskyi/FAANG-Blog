af2dad7de47d3711ce4384b1b56b527a0f47f115
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-07-11 18:57:24 +0300

    Made post request for user registration

--- faangblog_back/index.ts
+++ faangblog_back/index.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
+import { validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
+
+import UserModel from './models/User.js';
+
+import { registerValidation } from './validations/auth.js';
 
 config();
 mongoose
@@ -10,14 +16,32 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hi');
 });
 
-app.post('/auth/login', (req, res) => {
-  const token = jwt.sign(req, 'secret123');
+app.post('/auth/register', registerValidation, async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    passwordHash,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+  });
+
+  const user = await doc.save();
+
+  return res.json(user);
 });
 
-app.use(express.json());
 app.listen(3333);
---
+++ faangblog_back/validations/auth.ts
@@ -0,0 +1,7 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+  body('email', 'Incorrect e-mail format').isEmail(),
+  body('password', 'Your password length must be at least 5 symbols').isLength({ min: 5 }),
+  body('avatarUrl', 'Incorrect URL').optional().isURL(),
+];
